350ed9932295c5243e1f4e811eb9152eeb1837c1
Stanislav Zaleski <sigmaray@gmail.com>
2026-06-27 18:18:29 +0300

    Fix linter error

--- app/vcr_setup.py
+++ app/vcr_setup.py
@@ -1,11 +1,12 @@
 import os
+from typing import Any
 
 
 class _VCRContext:
-    def __init__(self):
-        self.cassette = None
+    def __init__(self) -> None:
+        self.cassette: Any | None = None
 
-    def start(self):
+    def start(self) -> None:
         if os.environ.get("USE_VCR") != "true":
             return
 
@@ -22,7 +23,7 @@ class _VCRContext:
         )
         self.cassette.__enter__()
 
-    def stop(self):
+    def stop(self) -> None:
         if self.cassette is not None:
             self.cassette.__exit__(None, None, None)
             self.cassette = None
@@ -31,10 +32,10 @@ class _VCRContext:
 vcr_context = _VCRContext()
 
 
-def apply_vcr_if_e2e():
+def apply_vcr_if_e2e() -> None:
     """Activates VCR if USE_VCR env var is true."""
     vcr_context.start()
 
 
-def stop_vcr():
+def stop_vcr() -> None:
     vcr_context.stop()
